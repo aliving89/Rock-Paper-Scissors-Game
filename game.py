5f9a4c9011413383ebda14de6e6afb8286cda8cc
Andrew - Fanshawe <alivingston89@gmail.com>
2022-03-23 10:26:45 -0400

    Building outlogic

--- game.py
+++ game.py
@@ -2,9 +2,11 @@ from random import randint
 
 choices = ["rock", "paper", "scissors"]
 
-player_choice = choices[1]
+player_lives = 3
+computer_lives = 3
+total_lives = 3
 
-# print("index 1 in the choice array is " + player_choice + ", which is paper")
+player_choice = choices[1]
 
 player_choice = input("Choose rock, paper, or scissors: ")
 
@@ -20,17 +22,26 @@ if player_choice == computer_choice:
 elif computer_choice == "rock":
     if player_choice == "scissors":
         print("You lose!")
+        player_lives -= 1
     else:
         print("You win!")
+        computer_lives -= 1
 
 elif computer_choice == "paper":
     if player_choice == "scissors":
         print("You win!")
+        computer_lives -= 1
     else:
         print("You lose!")
+        player_lives -= 1
 
 elif computer_choice == "scissors":
     if player_choice == "paper":
         print("You lose!")
+        player_lives -= 1
     else:
         print("You win!")
+        computer_lives -= 1
+
+print("Player lives:", player_lives)
+print("Computer lives:", computer_lives)
